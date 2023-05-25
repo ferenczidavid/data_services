9f1c4aa8546158bcd7ffce8200fbb72b2a2ae698
David Ferenczi <ferenczi.david55@gmail.com>
2023-05-25 16:08:28 +0200

    final home commit

--- src/features/Home.jsx
+++ src/features/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
         </div>
 
         {/* Col bottom Services container */}
-        <div className='mx-6 -translate bottom-5 flex flex-col py-8 bg-zinc-200 border border-slate-400 rounded-xl text-center shadow-xl items-center col-span-2'>
+        <div className='mx-6 -translate bottom-px flex flex-col py-8 bg-zinc-200 border border-slate-400 rounded-xl text-center shadow-xl items-center md:col-span-2'>
           <p className='underline cursor-default'>Services</p>
           <ul className='w-full whitespace-nowrap flex justify-center md:justify-between pt-2 flex-wrap gap-2'>
             {services.map((item, i) => (<li key={`service_item_${i}`} className='basis-[100px] justify-center gap-2 inline-flex  cursor-default'>{item.icon}{item.label}</li>))}
